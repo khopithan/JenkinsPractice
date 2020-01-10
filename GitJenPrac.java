4efbc691ed90134b7aebe14a722e1f9dd2f7a2bc
Khopithan <khopithankhopi@gmail.com>
2020-01-10 15:27:28 +0530

    first commit GitToJenkins

---
+++ GitJenPrac.java
@@ -0,0 +1,20 @@
+public class GitJenPrac{
+
+	public static void main(String[] args){
+		
+		System.out.println("ooi pisaasu");
+		System.out.println("hello madam");
+		System.out.println("ooi pisaasu");
+		System.out.println("hello madam");
+		System.out.println("ooi pisaasu");
+		System.out.println("hello madam");
+		System.out.println("ooi pisaasu");
+                System.out.println("hello madam");
+                System.out.println("ooi pisaasu");
+                System.out.println("hello madam");
+                System.out.println("ooi pisaasu");
+                System.out.println("hello madam");
+
+	}
+
+}
